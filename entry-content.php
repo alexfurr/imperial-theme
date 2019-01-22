9bd4f7220c28ae18c61abf03c5deec77ba1eff33
beacaballero <b.caballero-martin@imperial.ac.uk>
2019-01-22 09:46:41 +0000

    added date and author name and last name

--- entry-content.php
+++ entry-content.php
@@ -1,5 +1,21 @@
 <div class="entry-content">
 
+<!-- Add the author and date for posts -->
+
+<?php 
+$thedate = get_the_date();
+$fname = get_the_author_meta('first_name');
+$lname = get_the_author_meta('last_name');
+$thetitle=get_the_title(); 
+
+echo '<h2 class="post-title">'.$thetitle.'</h2>';
+echo '<span class="post-author">By '.$fname. ' ' .$lname.'</span>';
+echo '<span class="post-date"> | '.$thedate.'</span>';
+
+
+?>
+
+
 
 <?php the_content(); ?>
 
